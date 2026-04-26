71f2d0b737e536a61ada4983c67879aabe9d482d
wild-duck-in-woods <ajaykalindala@gmail.com>
2026-04-26 12:53:58 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import TaskInput from "./components/TaskInput";
-import TaskList from "./components/TaskList";
 import FilterButtons from "./components/FilterButtons";
 import { useState, useEffect } from "react";
 
@@ -11,7 +10,8 @@ function App() {
   })
   const [filter, setFilter] = useState("all");
   const [search, setSearch] = useState("");
-
+  const [editingIndex, setEdititingIndex] = useState(null);
+  const [editText, setEditText] = useState("");
 
 
 
@@ -19,6 +19,11 @@ function App() {
     localStorage.setItem("tasks", JSON.stringify(tasks));
   }, [tasks]);
 
+  const editTask = (index, newText) =>{
+    const newTasks = [...tasks];
+    newTasks[index].text = newText;
+    setTasks(newTasks);
+  };
   const toggleTask = (taksToDelete) => {
     const newTasks = tasks.map((t) =>
       t === taksToDelete ? { text: t.text, completed: !t.completed } : t
@@ -27,13 +32,37 @@ function App() {
   };
 
   const addTask = () => {
-    console.log("clicked"); // 👈 check this
+    //console.log("clicked"); // 👈 check this
     if(task.trim() === "") return;
-    setTasks([...tasks, {text: task, completed: false}]);
+
+    setTasks([
+      ...tasks,
+      {
+        text: task,
+         completed: false,
+          date: new Date.toLocaleString()
+      }
+    ]);
+
     setTask("");
   }
+
+  
   const deleteTask = (taskToDelete) =>{
     setTasks(tasks.filter((t)=> t!== taskToDelete));
+  }
+
+  const startEdit = (index, text)=>{
+    setEdititingIndex(index);
+    setEditText(text);
+  }
+
+  const saveEdit = (index)=>{
+    const newTasks = [...tasks];
+    newTasks[index].text = editText;
+    setTasks(newTasks);
+    setEdititingIndex(null);
+    setEditText("");
   }
   const filteredTasks = tasks.filter((t) => {
     const matchesFilter = 
@@ -52,8 +81,10 @@ function App() {
     <div className="container">
       <h1>To-Do App</h1>
 
-      <TaskInput task={task} setTask={setTask} addTask={addTask} />
+      <TaskInput task={task} setTask={setTask} addTask={addTask}  />
       
+      {tasks.length === 0 && <p> No tasks Yet</p>}
+
       <input
                 value={search}
                 onChange={(e)=>setSearch(e.target.value)}
@@ -61,12 +92,48 @@ function App() {
             />
       <FilterButtons filter= {filter} setFilter={setFilter} />
 
-      <TaskList
-        tasks={filteredTasks}
-        toggleTask={toggleTask}
-        deleteTask={deleteTask}
-      />
+       <ul>
+            {filteredTasks.map((t,index) => (
+                <li key={index}>
 
+                    {editingIndex === index ? (
+                        <>
+                            <input
+                                value={editText}
+                                onChange = {(e)=>setEditText(e.target.value)}
+                            />
+                            <button onClick={()=>saveEdit(index)}>save</button>
+                        </>
+                    ):(
+                       <>
+                            <span   
+                                onClick={() => toggleTask(t)}
+                                style={{
+                                cursor: "pointer",
+                                textDecoration: t.completed? "line-through":"none"
+                                }}
+                            >
+                        {t.text}
+                            </span>
+                            <br />
+                            <small> {t.date}</small>
+                            <br />
+
+                            <button onClick={()=>startEdit(index, t.text)}>
+                                edit
+                            </button>
+
+                            <button onClick={()=>deleteTask(t)}>
+                                delete
+                            </button>
+                       </> 
+                    )}
+                    
+            
+                </li>
+            ))}
+        </ul>
+      
       
     </div>
   )
--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,21 +1,42 @@
-function TaskList({ tasks, toggleTask, deleteTask }){
+function TaskList({ tasks, toggleTask, deleteTask, editingIndex, startEdit }){
     return (
         <ul>
             {tasks.map((t,index) => (
                 <li key={index}>
-                    <span   
-                        onClick={() => toggleTask(t)}
-                        style={{
-                            cursor: "pointer",
-                            textDecoration: t.completed? "line-through":"none"
-                        }}
-                    >
-                        {t.text};
-                    </span>
 
-                    <button onClick={()=> deleteTask(t)}>
-                        delete
-                    </button> 
+                    {editingIndex === index ? (
+                        <>
+                            <input
+                                value={editText}
+                                onChange = {(e)=>setEditText(e.target.value)}
+                            />
+                            <button onClick={()=>saveEdit(index)}>save</button>
+                        </>
+                    ):(
+                       <>
+                            <span   
+                                onClick={() => toggleTask(t)}
+                                style={{
+                                cursor: "pointer",
+                                textDecoration: t.completed? "line-through":"none"
+                                }}
+                            >
+                                {t.text}
+                            </span>
+                            <br />
+                             
+                            <br />
+
+                            <button onClick={()=>startEdit(index, t.text)}>
+                                edit
+                            </button>
+
+                            <button onClick={()=>deleteTask(t)}>
+                                delete
+                            </button>
+                       </> 
+                    )}
+                    
             
                 </li>
             ))}
